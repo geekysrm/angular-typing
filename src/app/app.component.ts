dc8386c1df64570f6d936cb47a117ea0b1ab1844
geekysrm <soumyacool2012@gmail.com>
2020-08-23 17:40:02 +0530

    complete app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-typing';
+  randomText = lorem.sentence();
+  userInputText = '';
+
+  onTextInput(value: string) {
+    this.userInputText = value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'wrong';
+  }
 }
